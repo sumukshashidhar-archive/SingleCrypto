53dda38354aa1bbc32931852ce27ea07a68aac62
sumukshashidhar <sumukuuu@gmail.com>
2021-09-12 02:04:05 -0500

    feat: add test folder delete

--- src/test_crypt.py
+++ src/test_crypt.py
@@ -9,18 +9,34 @@ import os
 def make_folders_and_files():
     """
     Generates a test folder with nested files.
+    :return: Boolean True or False
+    """
+    try:
+        os.mkdir("test/")
+        os.mkdir("test/testr/")
+        with open("test/a.txt", "w+") as f:
+            f.write("AAAAaaaa")
+        with open("test/testr/b.txt", "w+") as f:
+            f.write("BBBBbbbb")
+        return True
+    except OSError:
+        return False
+
+
+def delete_folders_and_files():
+    """
+    Folder to clean up test files.
     :return:
     """
-    os.mkdir("test/")
-    os.mkdir("test/testr/")
-    with open("test/a.txt", "w+") as f:
-        f.write("AAAAaaaa")
-    with open("test/testr/b.txt", "w+") as f:
-        f.write('BBBBbbbb')
+    try:
+        os.rmdir("test/")
+        os.rmdir("test_encrypted/")
+        return True
+    except OSError:
+        return False
 
 
 class Crypt(unittest.TestCase):
-
     def test_sanity(self):
         self.assertEqual("Hello", "Hello")
 
